76d52427440b683aed54582d4563277a4f734380
Ramiya Shree Seshaiah <43201743+Ramiyashree@users.noreply.github.com>
2024-03-09 17:31:19 -0600

    typo fix

--- components/index.js
+++ components/index.js
@@ -4,6 +4,6 @@ export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
 export { default as HeroBanner } from './HeroBanner';
 export { default as FooterBanner } from './FooterBanner';
-export { default as Cart } from './Cart';
 export { default as PaymentSuccessful } from './PaymentSuccessful';
 export { default as CheckoutPage } from './CheckoutPage';
+export { default as Cart } from './cart';
